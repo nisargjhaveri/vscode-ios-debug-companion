3e600e0fc217cb7143a54ad2e4807b7715aba03d
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2023-06-23 13:07:54 +0530

    Additionally use config option for permission to share

--- src/extension.ts
+++ src/extension.ts
@@ -2,13 +2,53 @@ import * as vscode from 'vscode';
 import * as logger from './logger';
 import { startUsbmuxdReverseProxy } from './usbmuxdReverseProxy';
 
-enum UsbmuxdProxyAllowed {
-    yes,
-    no,
-    ask
-};
+type UsbmuxdReverseProxyAllowed = "unknown" | "yes" | "no";
+type ConfigShareLocalDevices = "ask" | "always" | "never";
 
-let usbmuxdReverseProxyAllowed = UsbmuxdProxyAllowed.ask;
+let usbmuxdReverseProxyAllowed: UsbmuxdReverseProxyAllowed = "unknown";
+
+
+function ensureUserPermission(): boolean {
+    const iosDebugConfig = vscode.workspace.getConfiguration("ios-debug");
+    const configShareLocalDevices = iosDebugConfig.get<ConfigShareLocalDevices>('shareLocalDevices');
+
+    if (typeof configShareLocalDevices === "undefined") {
+        // ios-debug extension not installed? Or does not yet support sharing devices?
+        // We should not reach here.
+        return false;
+    }
+
+    if (configShareLocalDevices === "always") {
+        usbmuxdReverseProxyAllowed = "yes";
+        return true;
+    } else if (configShareLocalDevices === "never") {
+        usbmuxdReverseProxyAllowed = "no";
+        return false;
+    } else if (configShareLocalDevices === "ask") {
+        vscode.window.showInformationMessage(
+            'Allow debugging on locally connected iOS devices? This will make the devices accessible from the remote machine.',
+            {value: "yes", title: 'Yes'},
+            {value: "always", title: 'Always'},
+            {value: "never", title: 'Never'},
+        ).then(async (item) => {
+            if (typeof item === "undefined") {
+                usbmuxdReverseProxyAllowed = "no";
+            } else if (item.value === "yes") {
+                usbmuxdReverseProxyAllowed = "yes";
+            } else if (item.value === "always") {
+                usbmuxdReverseProxyAllowed = "yes";
+                await iosDebugConfig.update("shareLocalDevices", "always", true);
+            } else if (item.value === "never") {
+                usbmuxdReverseProxyAllowed = "no";
+                await iosDebugConfig.update("shareLocalDevices", "never", true);
+            }
+        });
+
+        return false;
+    }
+
+    return false;
+}
 
 export function activate(context: vscode.ExtensionContext) {
     logger.activate(vscode.window.createOutputChannel("iOS Debug Companion"));
@@ -20,20 +60,14 @@ export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(vscode.commands.registerCommand('ios-debug-companion.canStartUsbmuxdReverseProxy', (): boolean => {
         switch (usbmuxdReverseProxyAllowed) {
-            case UsbmuxdProxyAllowed.yes: {
+            case "yes": {
                 return true;
             }
-            case UsbmuxdProxyAllowed.ask: {
-                vscode.window.showInformationMessage(
-                    'Allow debugging on locally connected iOS devices? This will make the devices accessible from the remote machine.',
-                    {value: UsbmuxdProxyAllowed.yes, title: 'Yes'},
-                    {value: UsbmuxdProxyAllowed.no, title: 'No'},
-                ).then((item) => {
-                    usbmuxdReverseProxyAllowed = item?.value ?? UsbmuxdProxyAllowed.no;
-                });
-                return false;
+            case "unknown": {
+                // Ask for permission, don't wait for the response
+                return ensureUserPermission();
             }
-            case UsbmuxdProxyAllowed.no:
+            case "no":
             default: {
                 return false;
             }
@@ -41,7 +75,7 @@ export function activate(context: vscode.ExtensionContext) {
     }));
 
     context.subscriptions.push(vscode.commands.registerCommand('ios-debug-companion.startUsbmuxdReverseProxy', async (url: string) => {
-        if (usbmuxdReverseProxyAllowed !== UsbmuxdProxyAllowed.yes) {
+        if (usbmuxdReverseProxyAllowed !== "yes") {
             return;
         }
 
